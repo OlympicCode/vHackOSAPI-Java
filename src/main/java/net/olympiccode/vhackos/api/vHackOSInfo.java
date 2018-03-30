f0deaaa007e153ced8fe2c6f578f5efab68d7850
Checkium <checkium@gmail.com>
2018-03-30 18:27:01 +0100

    Easter update

--- src/main/java/net/olympiccode/vhackos/api/vHackOSInfo.java
+++ src/main/java/net/olympiccode/vhackos/api/vHackOSInfo.java
@@ -1,6 +1,6 @@
 package net.olympiccode.vhackos.api;
 
 public class vHackOSInfo {
-    private static final int REST_VERSION = 13;
+    private static final int REST_VERSION = 14;
     public static final String API_PREFIX = String.format("https://api.vhack.cc/mobile/%d/", REST_VERSION);
 }
